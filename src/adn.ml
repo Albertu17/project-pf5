8a5a59928390f1ff50c99e7ddd8adf650a3661f3
bencheih <hamame.bencheikh-lehocine@etu.u-paris.fr>
2023-11-02 15:32:24 +0100

    First commit, implemented parts 1, 2, 3, 4, 5, 6 of activity 3

--- src/adn.ml
+++ src/adn.ml
@@ -20,22 +20,49 @@ let string_of_base (b : base) : string =
 
 (* explode a string into a char list *)
 let explode (str : string) : char list =
-  failwith "À compléter"
-
+  let rec build_list s n ls = 
+    (*
+       s: the string to explode
+       n: the current index 
+       (starts from the length of the string and goes down to 0)
+       ls: the list to return
+       *)
+    if n<0 then ls
+    else build_list s (n-1) ((String.get s n)::ls)
+  in if str="" then [] else
+  build_list str ((String.length str)-1) []
 
 (* conversions *)
 let base_of_char (c : char) : base =
-  failwith "À compléter"
+  match c with
+  |'A' -> A
+  |'C' -> C
+  |'G' -> G
+  |'T' -> T
+  |_ -> WC
 
 
 let dna_of_string (s : string) : base list =
-  failwith "À compléter"
-
+  let rec build_list lc ldna = 
+    (*
+      lc: la liste des caractères extraits de la chaine 
+      ldna: la liste des bases DNA
+    *)
+    match lc with
+    |[] -> List.rev ldna
+    |a::r -> build_list r ((base_of_char a)::ldna)
+  in build_list (explode s) []
 
 let string_of_dna (seq : dna) : string =
-  failwith "À compléter"
-
-
+  let rec build_string ldna s = 
+    (*
+      ldna: la liste des bases ADN
+      s: la chaine a retourner
+    *)
+    match ldna with
+    |[] -> s
+    |d::r -> build_string r (s^(string_of_base d))
+  in build_string seq ""
 
 (*---------------------------------------------------------------------------*)
 (*                                   SLICES                                  *)
@@ -48,8 +75,17 @@ let string_of_dna (seq : dna) : string =
 
 (* if list = pre@suf, return Some suf. otherwise, return None *)
 let cut_prefix (slice : 'a list) (list : 'a list) : 'a list option =
-  failwith "A faire"
-
+  let rec build_suffix lst pre = 
+    (*
+       lst: la liste compléte
+       pre: le prefixe
+    *)
+    match lst with
+    |[]->if pre=[] then Some [] else None
+    |a::r -> if pre=[] then Some lst else
+      if a = (List.hd pre) then build_suffix r (List.tl pre)
+      else None
+  in build_suffix list slice
 (*
   cut_prefix [1; 2; 3] [1; 2; 3; 4] = Some [4]
   cut_prefix [1; 2; 3; 4] [1; 2; 3; 4] = Some []
@@ -60,9 +96,25 @@ let cut_prefix (slice : 'a list) (list : 'a list) : 'a list option =
 (* return the prefix and the suffix of the first occurrence of a slice,
    or None if this occurrence does not exist.
 *)
+
 let first_occ (slice : 'a list) (list : 'a list)
     : ('a list * 'a list) option =
-  failwith "À compléter"
+  let rec build (lst: 'a list) (cmp: 'a list) (pre: 'a list) (pos: 'a list)= 
+  (*
+     lst: la liste compléte
+     cmp: la liste recherché
+     pre: préfixe de la liste
+     suf: suffixe
+     pos: une liste pour contenir les éléments de la liste cmp une fois trouvés dans lst
+  *)
+    match lst with
+    |[] -> if cmp = [] then Some (pre, []) else None
+    |bs::r -> if cmp=[] then Some (pre, lst)
+    else 
+      if bs=(List.hd cmp) then build r (List.tl cmp) (pre) (pos@[bs])
+      else if cmp=slice then build r cmp (pre@[bs]) []
+      else build lst slice (pre@pos) []
+  in build list slice [] [] 
 (*
   first_occ [1; 2] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([1; 1], [3; 4; 1; 2])
   first_occ [1; 1] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([], [1; 2; 3; 4; 1; 2])
@@ -72,14 +124,19 @@ let first_occ (slice : 'a list) (list : 'a list)
 
 let rec slices_between
           (start : 'a list) (stop : 'a list) (list : 'a list) : 'a list list =
-  failwith "A faire"
+  match (first_occ start list) with 
+  |None -> []
+  |Some (x, y) -> 
+    match (first_occ stop y) with
+    |None -> []
+    |Some (z, t) -> z::(slices_between start stop t)
 
 (*
   slices_between [1; 1] [1; 2] [1; 1; 1; 1; 2; 1; 3; 1; 2] = [[1]; []; [2; 1; 3]]
  *)
 
 let cut_genes (dna : dna) : (dna list) =
-  failwith "A faire"
+  slices_between [A;T;G] [T;A;A] dna
 
 (*---------------------------------------------------------------------------*)
 (*                          CONSENSUS SEQUENCES                              *)
