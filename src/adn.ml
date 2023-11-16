0e24289c98a37ca1019c9f37d965a7e95657465a
bencheih <hamame.bencheikh-lehocine@etu.u-paris.fr>
2023-11-16 16:14:56 +0100

    Partie ADN terminée, peut avoir des problèmes ou des optimisations a faire ou possible d'ajouter des commentaires au programme

--- src/adn.ml
+++ src/adn.ml
@@ -26,7 +26,7 @@ let explode (str : string) : char list =
        n: the current index 
        (starts from the length of the string and goes down to 0)
        ls: the list to return
-       *)
+    *)
     if n<0 then ls
     else build_list s (n-1) ((String.get s n)::ls)
   in if str="" then [] else
@@ -114,7 +114,7 @@ let first_occ (slice : 'a list) (list : 'a list)
       if bs=(List.hd cmp) then build r (List.tl cmp) (pre) (pos@[bs])
       else if cmp=slice then build r cmp (pre@[bs]) []
       else build lst slice (pre@pos) []
-  in build list slice [] [] 
+  in build list slice [] []
 (*
   first_occ [1; 2] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([1; 1], [3; 4; 1; 2])
   first_occ [1; 1] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([], [1; 2; 3; 4; 1; 2])
@@ -132,7 +132,7 @@ let rec slices_between
     |Some (z, t) -> z::(slices_between start stop t)
 
 (*
-  slices_between [1; 1] [1; 2] [1; 1; 1; 1; 2; 1; 3; 1; 2] = [[1]]
+  slices_between [1; 1] [1; 2] [1; 1; 1; 1; 2; 1; 3; 1; 2] = [[1]; []; [2; 1; 3]]
  *)
 
 let cut_genes (dna : dna) : (dna list) =
@@ -149,28 +149,66 @@ type 'a consensus = Full of 'a | Partial of 'a * int | No_consensus
    (Partial (a, n)) if a is the only element of the list with the
    greatest number of occurrences and this number is equal to n,
    No_consensus otherwise. *)
-let consensus (list : 'a list) : 'a consensus =
-  let rec build_transpose ll tr n = 
-    "Nothing"
-  and 
-    count le a c g t =
-    (*
-      le: liste des éléments de la même colonne
-      a: compteur des bases A
-      c: compteur des bases C
-      g: compteur des bases G
-      t: compteur des bases T
-    *)
-    match le with
-    |[] -> (a, c, g, t)
-    |A::r -> count r (a+1) c g t
-    |C::r -> count r a (c+1) g t
-    |G::r -> count r a c (g+1) t
-    |T::r -> count r a c  g (t+1)
-    |_::r -> count r a c g t
-   
-  
-(*
+let consensus (list : 'a list) : 'a consensus = 
+  (*count the elements and add them in a list*)
+  let count (lst: 'a list): (int list * 'a list) = 
+    let rec build (lst: 'a list) (ints: int list) (alphas: 'a list) = 
+      (*
+      lst: list to process
+      ints: counts for each *base*
+      alphas: *bases*
+      *)
+      let rec count_one (lst: 'a list) (a: 'a): int = 
+        match lst with 
+        |[]->0
+        |x::r -> if x=a then 1+(count_one r a)
+        else count_one r a
+      in
+      match lst with
+      |[]->(ints, alphas)
+      |x::r -> if (List.mem x alphas) 
+        then build r ints alphas
+        else build r ((count_one lst x)::ints) (x::alphas)
+    in build lst [] []
+
+  and get_consensus ints alphas: ('a consensus) = 
+    let rec find_cons (ints: int list) (alphas: 'a list) (cons: ('a consensus) option) (cons_base: 'a) (cons_count: int) = 
+      match (ints, alphas) with
+      |([], []) -> let finish cons =  
+        match cons with 
+        |None->No_consensus
+        |Some cns -> cns
+      in finish cons
+      |(i::ri, a::ra) -> 
+        (*si le nombre est null, alors on ne change pas le consensus*)
+        if i=0 then find_cons ri ra cons cons_base cons_count 
+        (* si le nombre est inférieure a la valeur max (mais pas null), on change de Full a Partial *)
+        else if i<cons_count then match cons with
+          |Some Full(_) -> find_cons ri ra (Some(Partial(cons_base, cons_count))) cons_base cons_count
+          |_ -> find_cons ri ra cons cons_base cons_count
+        (*Si la valeur est égale a la valeur max, alors on change de Full (ou Partial) à No_consensus*)
+        else if i=cons_count then 
+          match cons with 
+          |None-> find_cons ri ra None a i
+          |Some _ -> find_cons ri ra (Some(No_consensus)) cons_base cons_count
+        (*Sinon (si la valeur est suppérieure), on change le consensus de la façon suivante
+            Full(ancien) -> Partial(Nouveau, valeur)
+            Partial(ancien, val)->Partial(Nouveau, valeur)
+            No_consensus -> Partial(Nouveau, valeur)
+            None -> Full(nouveau)*)
+        else 
+          let f cons = match cons with
+          |None->find_cons ri ra (Some(Full(a))) a i
+          |Some _ -> find_cons ri ra (Some(Partial(a, i))) a i
+        in f cons
+        |(_, _)->failwith"Impossible"
+    in find_cons ints alphas None (List.hd alphas) 0
+      
+  in match list with 
+  |[] -> No_consensus
+  |_ ->let pair = (count list) in (get_consensus (fst pair) (snd pair))
+
+  (*
    consensus [1; 1; 1; 1] = Full 1
    consensus [1; 1; 1; 2] = Partial (1, 3)
    consensus [1; 1; 2; 2] = No_consensus
@@ -182,10 +220,21 @@ let consensus (list : 'a list) : 'a consensus =
    are empty, return the empty sequence.
  *)
 
-let consensus_sequence (ll : 'a list list) : 'a consensus list =
-  failwith "A completer"
-
-(*
+let consensus_sequence (ll : 'a list list) : 'a consensus list = 
+  let rec get_nths lst n ret = 
+    match lst with
+      |[] -> ret
+      |a::r -> match (List.nth_opt a n) with 
+      |None->[]
+      |Some x -> get_nths r n (x::ret)
+  in 
+  let rec get_consensus_seq ll cnt len ret = 
+    if cnt = len then List.rev ret
+    else get_consensus_seq ll (cnt+1) len ((consensus (get_nths ll cnt []))::ret)
+  in if ll=[] then [] else
+  (get_consensus_seq ll 0 (List.length (List.hd ll)) [])
+  
+  (*
  consensus_sequence [[1; 1; 1; 1];
                      [1; 1; 1; 2];
                      [1; 1; 2; 2];
